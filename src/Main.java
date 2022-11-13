2d5ce74ff80354f9d054c6ca699cf8d3d8e0a7e7
Alp Senay <alps2789@nycstudents.net>
2022-11-13 13:00:16 -0500

    done!

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
         System.out.println(loops.countCharacters("a", "Apple and banana"));
         System.out.println(loops.countCharacters("A", "Apple and banana"));
         System.out.println(loops.countCharacters("!", "Hello! Nice day!"));
+        System.out.println(loops.reverseString("hello!"));
+        System.out.println(loops.reverseString("Apples and bananas"));
+
 
 
 
--- src/StringLoops.java
+++ src/StringLoops.java
@@ -8,21 +8,21 @@ public class StringLoops
         int count = 0;
         for (int idx = 0; idx < searchString.length(); idx++) {
             String nextChar = searchString.substring(idx, idx + 1);
-            if (nextChar.equals(character)) {
+            if (nextChar.equalsIgnoreCase(character)) {
                 count++;
             }
         }
         return count;
     }
 
-    /* Returns the original string reversed
 
-     Examples:
-     - if origString = "hello!" this method returns "!olleh"
-     - if origString = "Apples and bananas" this method returns "sananab dna selppA"
-    */
-    //public String reverseString(String origString) {
-        /* to be implemented */
-    //}
+    public String reverseString(String origString) {
+        String newString = "";
+        for (int idx = origString.length()-1; idx >= 0; idx--) {
+            String temp = origString.substring(idx, idx + 1);
+            newString += temp;
+        }
+        return newString;
+    }
 }
 
